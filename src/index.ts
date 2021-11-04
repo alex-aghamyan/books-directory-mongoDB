815ceea6579b9d0cfb88bdb4bced8bbc8eeddf66
alex-aghamyan <alex.aghamyan.work@gmail.com>
2021-11-04 13:28:36 +0400

    initial: project initial setup
    Create project structure, connect to MongoDB.

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config.js';
+import { booksRouter } from './routes/books.route.js';
+
+const PORT: number = Number(process.env.PORT);
+const DB_URI: string = process.env.DB_URI;
+
+const app = express();
+
+app.use(express.json());
+app.use('/api/books', booksRouter);
+
+async function run() {
+  await mongoose.connect(DB_URI);
+  await app.listen(PORT, () => {
+    console.log(`listening at port ${PORT}...`);
+  });
+}
+
+run();
